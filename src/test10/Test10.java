fa35411777f60cbf020248f6c285643ba0b43874
MariaVieira1234 <vieira.maria@gmail.com>
2017-09-17 20:00:59 -0700

    making changes

---
+++ src/test10/Test10.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test10;
+
+/**
+ *
+ * @author Maria Vieira
+ */
+public class Test10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test10\n");
+        System.out.println("test10.1\n");
+    }
+    
+}
